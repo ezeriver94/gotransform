01869c8d99e47d06eefdc9b8d225767c13655eec
Ezequiel Roman Farji <eze_farji@hotmail.com>
2020-07-05 19:56:00 -0300

    added functionality for extract, load and transform

---
+++ common/extract.go
@@ -0,0 +1,120 @@
+package common
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+// ValidateInt checks if one value can be converted to int
+func (f *Field) ValidateInt(data interface{}) (int, error) {
+	switch data.(type) {
+	case string:
+		result, err := strconv.Atoi(fmt.Sprintf("%v", data))
+		if err != nil {
+			return 0, err
+		}
+		return result, nil
+	case int:
+		return data.(int), nil
+	default:
+		return 0, fmt.Errorf("cannot convert from type %T to int", data)
+	}
+}
+
+// ValidateString checks if one value can be converted to string
+func (f *Field) ValidateString(data interface{}) (string, error) {
+	return data.(string), nil
+}
+
+// ValidateBool checks if one value can be converted to bool
+func (f *Field) ValidateBool(data interface{}) (bool, error) {
+	switch data.(type) {
+	case string:
+		result, err := strconv.ParseBool(fmt.Sprintf("%v", data))
+		if err != nil {
+			return false, err
+		}
+		return result, nil
+	case bool:
+		return data.(bool), nil
+	case int:
+		if data.(int) == 0 {
+			return false, nil
+		} else if data.(int) == 1 {
+			return true, nil
+		}
+		return false, fmt.Errorf("cannot convert int value %v to bool; allowed int values are 0 or 1", data)
+	case float32:
+		if data.(float32) == 0 {
+			return false, nil
+		} else if data.(float32) == 1 {
+			return true, nil
+		}
+		return false, fmt.Errorf("cannot convert float32 value %v to bool; allowed int values are 0 or 1", data)
+	case float64:
+		if data.(float64) == 0 {
+			return false, nil
+		} else if data.(float64) == 1 {
+			return true, nil
+		}
+		return false, fmt.Errorf("cannot convert float32 value %v to bool; allowed int values are 0 or 1", data)
+	default:
+		return false, fmt.Errorf("cannot convert from type %T to bool", data)
+	}
+}
+
+// Validate validates the data acording to the field spec
+func (f *Field) Validate(data interface{}) (interface{}, error) {
+	switch f.ExpectedType {
+	case "int":
+		return f.ValidateInt(data)
+	case "string":
+		return f.ValidateString(data)
+	case "bool":
+		return f.ValidateBool(data)
+	default:
+		return nil, fmt.Errorf("unknown type %v in field %v", f.ExpectedType, f.Name)
+	}
+}
+
+// FieldCount returns the amount of fields defined within a datasource
+func (ds *DataEndpoint) FieldCount() int {
+	return len(ds.Fields)
+}
+
+// Validate deserializes json data into an array and checks every field against the attributes of the metadata instance
+func (ds *DataEndpoint) Validate(jsonData []byte) (map[string]interface{}, error) {
+	errString := ""
+	fieldCount := ds.FieldCount()
+	parsed := make([]interface{}, fieldCount)
+
+	err := json.Unmarshal(jsonData, &parsed)
+	if err != nil {
+		return nil, fmt.Errorf("error deserializing json: %v", err)
+	}
+	if len(parsed) < ds.FieldCount() {
+		return nil, fmt.Errorf("row length (%v) is less than metadata fields (%v)", len(parsed), fieldCount)
+	}
+	if len(parsed) > ds.FieldCount() {
+		return nil, fmt.Errorf("row length (%v) is greater than metadata fields (%v)", len(parsed), fieldCount)
+	}
+
+	result := make(map[string]interface{}, fieldCount)
+
+	for index, field := range ds.Fields {
+		converted, err := field.Validate(parsed[index])
+		if err != nil {
+			errString = fmt.Sprintf("%v\n%v", errString, err)
+		} else {
+			result[field.Name] = converted
+		}
+	}
+
+	if errString != "" {
+		return nil, errors.New(errString)
+	}
+
+	return result, nil
+}
--- common/metadata.go
+++ common/metadata.go
@@ -1,116 +1,65 @@
 package common
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"strconv"
 
 	"gopkg.in/yaml.v2"
 )
+
+// DataEndpoint contains information of a single entity which acts both as a source and as a data destination
+type DataEndpoint struct {
+	Driver           string  `yaml:"driver"`
+	ConnectionString string  `yaml:"connectionstring"`
+	ObjectIdentifier string  `yaml:"objectid"`
+	Fields           []Field `yaml:"fields"`
+}
+
+// Join represents a way to join two datasources
+type Join struct {
+	To string   `yaml:"to"`
+	On []string `yaml:"on"`
+}
+
+// SelectClause contains the way to obtain a value from a datasource. format: datasource.fieldname
+type SelectClause string
+
+// DataTransformation defines the directives to use to handle a transformation on one or multiple datasources
+type DataTransformation struct {
+	From   string                  `yaml:"from"`
+	Joins  map[string]Join         `yaml:"joins"`
+	Where  []string                `yaml:"where"`
+	Select map[string]SelectClause `yaml:"select"`
+}
 
 // Field represents the attributes of a field of the Metadata
 type Field struct {
 	Name         string `yaml:"name"`
 	ExpectedType string `yaml:"type"`
+	FixedLength  int    `yaml:"fixedlength"`
+	MaxLength    int    `yaml:"maxlength"`
+	EndCharacter string `yaml:"endchar"`
+}
+
+// Extract contains primary datasources (which are fully read and cannot be related to each others) and aditional datasources (used on join clauses on transformations)
+type Extract struct {
+	PrimaryDataSources   map[string]DataEndpoint `yaml:"primary"`
+	AditionalDataSources map[string]DataEndpoint `yaml:"aditional"`
 }
 
 // Metadata represents the definition of a single row of data
 type Metadata struct {
-	Fields []Field `yaml:"fields"`
+	Version   string                        `yaml:"version"`
+	Extract   Extract                       `yaml:"extract"`
+	Transform map[string]DataTransformation `yaml:"transform"`
+	Load      map[string]DataEndpoint       `yaml:"load"`
 }
 
 // ParseMetadata converts a string to a Metadata instance
 func ParseMetadata(data string) (*Metadata, error) {
 	var result = new(Metadata)
 	err := yaml.UnmarshalStrict([]byte(data), &result)
-	return result, err
-}
-
-// FieldCount returns the amount of fields of a metadata instance
-func (m *Metadata) FieldCount() int {
-	return len(m.Fields)
-}
-
-// Validate checks a fields data and returns error if it cannot be converted to the expected type
-func (f *Field) Validate(data interface{}) (interface{}, error) {
-	var isOk = true
-	var result interface{} = nil
-	var err error
-
-	switch f.ExpectedType {
-	case "int":
-		switch data.(type) {
-		case string:
-			result, err = strconv.Atoi(fmt.Sprintf("%v", data))
-			if err != nil {
-				isOk = false
-			}
-		case int:
-			result = data.(int)
-			isOk = true
-		default:
-			isOk = false
-		}
-	case "string":
-		result = data.(string)
-		isOk = true
-	case "bool":
-		switch data.(type) {
-		case string:
-			result, err = strconv.ParseBool(fmt.Sprintf("%v", data))
-			if err != nil {
-				isOk = false
-			}
-		case bool:
-			result = data.(bool)
-			isOk = true
-		case int:
-			result = data.(int)
-			if !(result == 0 || result == 1) {
-				isOk = false
-			}
-		}
-	default:
-		return nil, fmt.Errorf("unexpected type %v of field %v", f.ExpectedType, f.Name)
-	}
-
-	if !isOk {
-		return nil, fmt.Errorf("cannot convert field %v with value %v to %v", f.Name, data, f.ExpectedType)
-	}
-	return result, nil
-}
-
-// Validate deserializes json data into an array and checks every field against the attributes of the metadata instance
-func (m *Metadata) Validate(jsonData []byte) ([]interface{}, error) {
-	errString := ""
-	parsed := make([]interface{}, m.FieldCount())
-
-	err := json.Unmarshal(jsonData, &parsed)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error deserializing metadata as yaml: %v", err)
 	}
-	if len(parsed) < m.FieldCount() {
-		return nil, fmt.Errorf("row length (%v) is less than metadata fields (%v)", len(parsed), m.FieldCount())
-	}
-	if len(parsed) > m.FieldCount() {
-		return nil, fmt.Errorf("row length (%v) is greater than metadata fields (%v)", len(parsed), m.FieldCount())
-	}
-
-	result := make([]interface{}, m.FieldCount())
-
-	for index, field := range m.Fields {
-		converted, err := field.Validate(parsed[index])
-		if err != nil {
-			errString = fmt.Sprintf("%v\n%v", errString, err)
-		} else {
-			result[index] = converted
-		}
-	}
-
-	if errString != "" {
-		return nil, errors.New(errString)
-	}
-
 	return result, nil
 }
